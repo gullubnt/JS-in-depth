6d564588d01187a1dfc676bc399d86b9f6f33d52
Abhishek Kr. Sharma <abhisheksharma12891@gmail.com>
2024-11-10 09:21:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Student Detail");
+let name = "Abhishek";
+let year = 3;
+let branch = "CSE";
+let section = "A";
+let userIntro =
+  "my details = " + name + " " + year + " " + branch + " " + section;
